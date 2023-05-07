1e60fa5a8bda228839806f53114a2e07a06d03ee
Yen Jung Chen <jacky010080@gmail.com>
2023-05-07 22:34:18 +0800

    資料更新

--- app.js
+++ app.js
@@ -6,4 +6,26 @@ window.addEventListener("scroll",() => {
   } else {
     nav.style.boxShadow = "0 10px 6px -6px #777";
   }
-})
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination"
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar"
+  }
+});
